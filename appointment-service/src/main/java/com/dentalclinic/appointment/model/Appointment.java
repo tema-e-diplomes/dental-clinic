94dffc694c78cbae529524e9c81dd4000a8df11e
Drilon Sadikaj <drilonsadikaj67304@gmail.com>
2026-03-07 16:06:40 +0100

    feat: add entity models for each microservice + docker compose to try connection with database

---
+++ appointment-service/src/main/java/com/dentalclinic/appointment/model/Appointment.java
@@ -0,0 +1,27 @@
+package com.dentalclinic.appointment.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "appointments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID patientId;
+    private UUID doctorId;
+    private LocalDateTime appointmentTime;
+    private String reason;
+    private String status;
+}
